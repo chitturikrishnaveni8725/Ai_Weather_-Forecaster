503d630bcbf7206c27fad94a19363723724c4a53
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-06-06 12:05:22 +0530

    after adding fronted  requirements.txt

--- fronted/app.py
+++ fronted/app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 import requests
-base_url="http://127.0.0.1:8000"
+base_url="https://ai-weather-forecaster-1.onrender.com"
 st.title("🌤️ AI Weather Forecaster")
 city=st.text_input("Enter City")
 question=st.text_input("Ask Your Weather Question")
