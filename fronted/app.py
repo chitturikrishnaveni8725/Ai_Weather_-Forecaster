049fbbcc51181c2f3187e6182e16549188282cc1
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-06-06 13:13:44 +0530

    after adding requirments.txt

--- fronted/app.py
+++ fronted/app.py
@@ -1,26 +1,24 @@
 import streamlit as st 
 import requests
-base_url="https://ai-weather-forecaster-2.onrender.com"
+base_url="http://127.0.0.1:8000"
 st.title("🌤️ AI Weather Forecaster")
 city=st.text_input("Enter City")
 question=st.text_input("Ask Your Weather Question")
 
 
 if st.button("ASK AGENT"):
-    res=requests.post(f"{base_url}/get_weather",json={
+    res=requests.post(f"{base_url}/get_weather",params={
                       
                       "city":city,
                       "question":question
                       
                       })
-    st.write("URL:", base_url)
-    st.write(res.status_code)
-    st.write(res.text)
-    # data = res.json()
-    # st.success(data["messages"][-1]["content"])
+  
+    data = res.json()
+    # st.write(data)  
+    st.success(data["messages"][-1]["content"])
    
   
     
-
 
 
