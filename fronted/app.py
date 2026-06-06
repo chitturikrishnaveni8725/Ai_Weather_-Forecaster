3f00465ce12f9b9cbfb9bb84f723d673e382f2c3
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-06-06 22:43:18 +0530

    after adding requirments.txt

--- fronted/app.py
+++ fronted/app.py
@@ -8,19 +8,35 @@ city=st.text_input("Enter City")
 question=st.text_input("Ask Your Weather Question")
 
 
-if st.button("ASK AGENT"):
-    res=requests.post(f"{BASE_URL}/get_weather",params={
+# if st.button("ASK AGENT"):
+#     res=requests.post(f"{BASE_URL}/get_weather",params={
                       
-                      "city":city,
-                      "question":question
+#                       "city":city,
+#                       "question":question
                       
-                      })
+#                       })
   
-    data = res.json()
-    # st.write(data)  
-    st.success(data["messages"][-1]["content"])
+#     data = res.json()
+#     # st.write(data)  
+#     st.success(data["messages"][-1]["content"])
    
-  
+if st.button("ASK AGENT"):
+    res = requests.post(
+        f"{BASE_URL}/get_weather",
+        json={
+            "city": city,
+            "question": question
+        }
+    )
+
+    st.write("STATUS:", res.status_code)
+    st.write("RESPONSE:", res.text)
+
+    try:
+        data = res.json()
+        st.success(data["messages"][-1]["content"])
+    except:
+        st.error("Backend did not return JSON")  
     
 
 
