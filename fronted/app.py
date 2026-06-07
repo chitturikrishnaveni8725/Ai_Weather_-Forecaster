8927ae898c4eeb467da278d576a897817113e0dc
krishnaveni <chitturikrishnaveni8725@gmail.com>
2026-06-07 23:00:58 +0530

    after adding requirments.txt

--- fronted/app.py
+++ fronted/app.py
@@ -1,68 +1,18 @@
-# import streamlit as st 
-# import requests
-# # base_url="http://127.0.0.1:8000"
-# # base_url="https://ai-weather-forecaster-1.onrender.com"
-# BASE_URL="https://ai-weather-forecaster-1.onrender.com"
-# st.title("🌤️ AI Weather Forecaster")
-# city=st.text_input("Enter City")
-# question=st.text_input("Ask Your Weather Question")
-
-
-# if st.button("ASK AGENT"):
-#     res=requests.post(f"{BASE_URL}/get_weather",params={
-                      
-#                       "city":city,
-#                       "question":question
-                      
-#                       })
-  
-#     data = res.json()
-#     # st.write(data)  
-#     st.success(data["messages"][-1]["content"])
-   
-# if st.button("ASK AGENT"):
-#     res = requests.post(
-#         f"{BASE_URL}/get_weather",
-#         params={
-#             "city": city,
-#             "question": question
-#         }
-#     )
-
-#     st.write("STATUS:", res.status_code)
-#     st.write("RESPONSE:", res.text)
-
-#     try:
-#         data = res.json()
-#         st.success(data["messages"][-1]["content"])
-#     except:
-#         st.error("Backend did not return JSON")  
 import streamlit as st 
 import requests
-
-BASE_URL = "https://ai-weather-forecaster-1.onrender.com"
-
+BASE_URL="http://127.0.0.1:8000"
 st.title("🌤️ AI Weather Forecaster")
+city=st.text_input("Enter City")
+question=st.text_input("Ask Your Weather Question")
 
-city = st.text_input("Enter City")
-question = st.text_input("Ask Your Weather Question")
 
-if st.button("ASK AGENT"):
-    res = requests.post(
-        f"{BASE_URL}/get_weather",
-        params={
-            "city": city,
-            "question": question
-        }
-    )
-
+if st.button("Ask Agent"):
+    res=requests.post(f"{BASE_URL}/get_weather",params={
+        "city":city,
+        "question":question
+    })
+    
     st.write("STATUS:", res.status_code)
-    st.write("RESPONSE:", res.text)
+    st.write("RAW RESPONSE:", res.text)
 
-    try:
-        data = res.json()
-        st.success(data["response"])   # ✅ FIXED HERE
-    except:
-        st.error("Backend did not return JSON")
-
-
+    # st.success(res.json()["messages"][-1]["content"])
